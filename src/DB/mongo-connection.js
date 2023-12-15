d163d4283d63810fb0a53cb8710f0ce9f285a210
ballo <ballo@localhost.localdomain>
2023-12-15 22:43:22 +0200

    update sc file

--- src/DB/mongo-connection.js
+++ src/DB/mongo-connection.js
@@ -1,17 +1,18 @@
+
 import mongoose from "mongoose";
 
-const mongoService =  process.env.mongo_service || '172.17.0.2'
+const mongoService =  process.env.mongo_service || 'mongo'
 const mongoUserName = process.env.mongo_userName ||'admin'
 const mongoPassword = process.env.mongo_password || 'admin'
 const mongoPort = process.env.mongo_port || 27017
 
 export const DB =  async ()=>{
     try{
-        await mongoose.connect(`mongodb://${mongoUserName}:${mongoPassword}@${mongoService}:${mongoPort}/ballo?readPreference=nearest&replicaSet=rs0&authSource=admin`);
+        await mongoose.connect(`mongodb://${mongoUserName}:${mongoPassword}@${mongoService}:${mongoPort}/ballo?authSource=admin`);
         console.log("connected to mongoDB")
 
     } catch (err){
-        console.log(`err in connectiong to the DB : ${err}`)
+        console.log(`err in connecting to the DB : ${err}`)
     }
 
 } 
